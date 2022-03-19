7fea19bed18ac173ea5261cade540044d50f77e6
leonardo cabral <leocds13@gmail.com>
2022-03-19 12:00:02 -0300

    Implemented Responsive LoginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:alura_geek/config/palette.dart';
+import 'package:alura_geek/config/responsive.dart';
 import 'package:alura_geek/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -31,38 +32,42 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Palette.offsetWhite,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 100,
-          horizontal: 40.0,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Iniciar sessão",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
+      child: Center(
+        child: SizedBox(
+          width: Responsive.isDescktop(context: context) ? 423 : 275,
+          height: Responsive.isMobile(context: context)
+              ? 362
+              : Responsive.isTablet(context: context)
+                  ? 295
+                  : 473,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Iniciar sessão",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            const SizedBox(height: 12),
-            const CustomTextField(
-              keyboardType: TextInputType.emailAddress,
-              labelText: "Escreva seu email",
-            ),
-            const SizedBox(height: 16),
-            const CustomTextField(
-              keyboardType: TextInputType.emailAddress,
-              labelText: "Escreva seu email",
-            ),
-            const SizedBox(height: 20),
-            SquareButton(
-              label: "Entrar",
-              color: Palette.primaryBlue,
-              onPressed: () {},
-            )
-          ],
+              const SizedBox(height: 12),
+              const CustomTextField(
+                keyboardType: TextInputType.emailAddress,
+                labelText: "Escreva seu email",
+              ),
+              const SizedBox(height: 16),
+              const CustomTextField(
+                keyboardType: TextInputType.emailAddress,
+                labelText: "Escreva seu email",
+              ),
+              const SizedBox(height: 20),
+              SquareButton(
+                label: "Entrar",
+                color: Palette.primaryBlue,
+                onPressed: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
